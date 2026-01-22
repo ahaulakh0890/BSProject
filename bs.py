245b1eb71dc5b27d2c5c4257fbf563139a043679
ahualakh0890 <ahaulakh9800@gmail.com>
2026-01-22 21:28:52 +0500

    Links are scrapped successfully

--- bs.py
+++ bs.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup   
 import requests 
 
-website = "https://subslikescript.com/movie/Titanic-120338"
+website = "https://subslikescript.com/movies"
 
 response = requests.get(website)
 
@@ -9,15 +9,33 @@ content = response.text
 
 soup = BeautifulSoup(content, 'html.parser')  
 
-titles = soup.find_all('p', class_="cue-line") 
-# for title in titles:
-#     print(title.get_text()) 
+box = soup.find('article', class_="main-article")   
 
-print("The whole script is printed above")   
+link_section = box.find_all('a', href=True)
 
-with open("titanic_script.txt", "w", encoding='utf-8') as f:
-    for title in titles:
-        f.write(title.get_text() + "\n")
+links = []
+
+for link in link_section:
+    links.append(link['href'])
+
+print("The links are being printed below:")
+
+for link in links:
+    print("https://subslikescript.com" + link)
+
+
+print("\n")
+print("The links are printed successfully")
+
+# titles = soup.find_all('p', class_="cue-line") 
+# # for title in titles:
+# #     print(title.get_text()) 
+
+# print("The whole script is printed above")   
+
+# with open("titanic_script.txt", "w", encoding='utf-8') as f:
+#     for title in titles:
+#         f.write(title.get_text() + "\n")
 
 
 # i = 0  
